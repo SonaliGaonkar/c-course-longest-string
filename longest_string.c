e97920ee87e6bb510c11f78794703d2d99be3733
Sonali Gaonkar <sonali.gaonkar@cctech.co.in>
2022-05-05 22:56:04 -0700

    update changes

--- longest_string.c
+++ longest_string.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        puts(" ");
-    }
-    else {
-        int arguments = 1;
-        for (int i = 2; i < argc; i++)  {
-            if(strlen(argv[arguments])<strlen(argv[i])) {
-                arguments = i;
-            }
+int main(int argc, char *argv[])
+{
+    char *arguments = "";
+    for (int i = 1; i < argc; i++)
+    {
+        if (strlen(arguments) < strlen(argv[i]))
+        {
+            arguments = argv[i];
         }
-        puts(argv[arguments]);
     }
+    puts(arguments);
     return EXIT_SUCCESS;
 }
